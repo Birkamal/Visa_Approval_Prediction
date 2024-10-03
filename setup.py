8f16eb19fc2140ee3e69663a9f0e05f46b9d6c63
Birkamal <dhillon.birkamal@gmail.com>
2024-10-03 17:03:14 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Visa_Prediction", #project name as you chose in template.py
+    version="0.0.0",#project version
+    author="Birkamal",
+    author_email="dhillon.birkamal@gmail.com",
+    packages=find_packages()
+)
