2a68e5c4900d758a38411cd60b193564f9c8df57
Birkamal <dhillon.birkamal@gmail.com>
2024-10-24 17:19:04 -0400

    Exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
 from Visa_Prediction.logger import logging
+from Visa_Prediction.exception import USvisaException
+import sys
 
-logging.info("Welcome to our custom log")
+
+'''Demo for logger'''
+#logging.info("Welcome to our custom log")
+
+'''Demo for exception'''
+try:
+    a=1/"12"
+except Exception as e:
+    logging.info(e) # to get error message in logger, helps during production
+    raise USvisaException(e, sys) from e # we trace the issue with the help of sys
